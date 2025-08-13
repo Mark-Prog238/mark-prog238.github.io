5695d524ea84e7e496c2d31a54f5bb89ff37c284
Mark Salamon <markmoneybag@Marks-MacBook-Air.local>
2025-08-13 14:37:36 +0200

    Deploy built files to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true, // Ensure paths work correctly on GitHub Pages
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
